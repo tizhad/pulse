44697570fc850b6ece9cde5651ff70077bf37db1
tina <tinarezanezhaad@gmail.com>
2026-05-01 15:08:12 +0200

    refactor: remove unused RouterLink and RouterLinkActive imports from app component

--- apps/job-mate/src/app/app.ts
+++ apps/job-mate/src/app/app.ts
@@ -1,11 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterModule, RouterLink, RouterLinkActive } from '@angular/router';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [RouterModule, RouterLink, RouterLinkActive],
+  imports: [RouterModule],
 })
 export class App {}
